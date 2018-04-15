931847f9200076c591bee1a9aeaafbeb4dc3c15b
Andrey Ka <kamish820@gmail.com>
2018-04-15 16:10:00 +0300

    Site version 0.0.0.1 beta

--- index.php
+++ index.php
@@ -44,22 +44,7 @@ printTree();
 echo "</pre>";
 
 
-/*$dir = "D:/Тест";
-$slash = "/";
-chdir($dir);
-$d = @opendir(".");
-    while (($e=readdir($d)) !==false)
-    {
-        if ($e=='.' || $e=='..') continue;
-        if (!@is_dir($e)) continue;
-        //for ($i=0; $i<$level; $i++) echo " /";
-        echo $dir.$slash.$e."<br>";
-        //if (!chdir($e)) continue;
-        //printTree($level+1);
-        //chdir("..");
-        //flush();
-    }
-*/
+
 
 
 ?>
